c4075e0f2ba882d5b01115765d6654f8000fe86e
Xoferif <Sniffern@msn.com>
2014-12-04 11:25:25 +0100

    altered a test, and added a new test for email retrieval.

--- TestMailClient/UnitTest1.cs
+++ TestMailClient/UnitTest1.cs
@@ -11,24 +11,36 @@ namespace TestMailClient
         [TestMethod]
         public void TestRetrieveEmails()
         {
+            //This test is to verify that retrieving the Email list actually works
             List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("pop.gmail.com", 995, true, "tgdxof@gmail.com", "MailClient");
 
         }
         [TestMethod]
-        [ExpectedException(typeof(FormatException))]
+        [ExpectedException(typeof(ArgumentException))]
         public void TestRetrieveEmailsWrongHostname()
         {
+            //testing Hostname is null, empty or only contains whitespaces.
             List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("", 995, true, "tgdxof@gmail.com", "MailClient");
 
         }
+
         [TestMethod]
         [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void TestRetrieveEmailsWithNoOrMissingSSL()
+        public void TestRetrieveEmailsWithNoOrMissingSslPort()
         {
+            //testing SslPort, if not 995, throw exception.
             List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("pop.gmail.com", 996, true, "tgdxof@gmail.com", "MailClient");
 
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRetrieveEmailsWithDisabledSsl()
+        {
+            //testing SslPort, if not 995, throw exception.
+            List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("pop.gmail.com", 995, false, "tgdxof@gmail.com", "MailClient");
+
+        }
 
 
     }
