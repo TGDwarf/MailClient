0655f73c40eae9b964a7aebd1ffc2c4b7ec70f0f
Xoferif <Sniffern@msn.com>
2014-12-04 11:09:01 +0100

    Created the first 3 small test for mail retrieval

---
+++ TestMailClient/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MailClient;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMailClient
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRetrieveEmails()
+        {
+            List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("pop.gmail.com", 995, true, "tgdxof@gmail.com", "MailClient");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TestRetrieveEmailsWrongHostname()
+        {
+            List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("", 995, true, "tgdxof@gmail.com", "MailClient");
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestRetrieveEmailsWithNoOrMissingSSL()
+        {
+            List<OpenPop.Mime.Message> EmailList = OpenPopParser.getAllMessages("pop.gmail.com", 996, true, "tgdxof@gmail.com", "MailClient");
+
+        }
+
+
+
+    }
+}
