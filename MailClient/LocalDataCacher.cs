5c4582820784d21594fa9084a2e0dd1494084ff0
TGDwarf <tgdwarf@gmail.com>
2014-12-04 09:20:09 +0100

    Added local cache to be used with export of xml

---
+++ MailClient/LocalDataCacher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+public class LocalDataCacher
+{
+    private static string StoragePath = @"C:\ProgramData\MailClient";
+
+    public static void Startup()
+    {
+
+        if (Directory.Exists(StoragePath))
+        {
+            Console.WriteLine("Storagepath " + StoragePath + " already existed");
+        }
+        else
+        {
+            Directory.CreateDirectory(StoragePath);
+            Console.WriteLine("Created " + StoragePath + " Successfully");
+
+        }
+
+        #region XML
+
+        using (XmlWriter writer = XmlWriter.Create(StoragePath + "\\Postal.xml"))
+        {
+            writer.WriteStartElement("Emails");
+            foreach (var PostalCode in MailList)
+            {
+
+                writer.WriteStartElement("Postal");
+                writer.WriteElementString("Id", Convert.ToString(PostalCode.Id));
+                writer.WriteElementString("Code", Convert.ToString(PostalCode.Code));
+                writer.WriteElementString("CityName", PostalCode.CityName);
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+            writer.Flush();
+        }
+        Console.WriteLine("Created Mail.xml Successfully");
+
+        #endregion XML
+
+        Console.ReadLine();
+    }
+}
