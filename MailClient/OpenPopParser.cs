40a1d8b03d34a53627592887f4fb3cef9cc53d99
Xoferif <Sniffern@msn.com>
2014-12-08 15:07:53 +0100

    deleted some trash

--- MailClient/OpenPopParser.cs
+++ MailClient/OpenPopParser.cs
@@ -11,28 +11,6 @@ using OpenPop.Mime;
 
 namespace MailClient
 {
-    [Serializable]
-    public class Email
-    {
-        public Email()
-        {
-            this.Attachments = new List<Attachment>();
-        }
-        public int MessageNumber { get; set; }
-        public string From { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-        public DateTime DateSent { get; set; }
-        public List<Attachment> Attachments { get; set; }
-    }
-
-    [Serializable]
-    public class Attachment
-    {
-        public string FileName { get; set; }
-        public string ContentType { get; set; }
-        public byte[] Content { get; set; }
-    }
     public partial class OpenPopParser
     {
         /// <summary>
