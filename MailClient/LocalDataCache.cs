3c9a0661383c46058a3cf11dcf8fbd42abeeb2ab
Xoferif <Sniffern@msn.com>
2014-12-10 08:54:20 +0100

    corrected a commit

---
+++ MailClient/LocalDataCache.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MailClient
+{
+    public partial class LocalDataCache
+    {
+        private static string xmlLocation = @"c:\ProgramData\MailClient\";
+        public static void saveMailsToXml()
+        {
+            if (Directory.Exists(xmlLocation))
+            {
+                if (File.Exists(xmlLocation + "incommingMails.xml"))
+                {
+                    File.Delete(xmlLocation + "incommingMails.xml");
+                }
+                if (File.Exists(xmlLocation + "sentMails.xml"))
+                {
+                    File.Delete(xmlLocation + "sentMails.xml");
+                }
+
+            }
+            else
+            {
+                Directory.CreateDirectory(xmlLocation);
+            }
+
+            using (XmlWriter writer = XmlWriter.Create(xmlLocation + "AllMails.xml"))
+            {            
+                writer.WriteStartElement("RegExes");
+                foreach (var RegEx in RegExesList)
+                {
+                    writer.WriteStartElement("RegEx");
+                    writer.WriteElementString("Name", RegEx.Name);
+                    writer.WriteElementString("Expression", RegEx.Expression);
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+   
+        }
+
+    }
+}
---
+++ TestMailClient/TestEmailDelete.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MailClient;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMailClient
+{
+    [TestClass]
+    public class TestEmailDelete
+    {
+        [TestMethod]
+
+        public void TestDeleteOfEmail()
+        {
+            TestStarter.updateUsers();
+            List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>();
+            List<string> messageIds = new List<string>();
+            allMessages = OpenPopParser.getAllMessages();
+
+            OpenPop.Pop3.Pop3Client client = new OpenPop.Pop3.Pop3Client();
+            client.Connect("pop.gmail.com",995,true);
+            client.Authenticate("recent:" + Users.username, Users.password);
+            bool result = OpenPopParser.DeleteMessageByMessageId(client, allMessages[0].Headers.MessageId);
+            client.Disconnect();
+        }
+        [TestMethod]
+        public void TestDeleteOfMultibleEmail()
+        {
+            TestStarter.updateUsers();
+            List<OpenPop.Mime.Message> allMessages = new List<OpenPop.Mime.Message>();
+            List<string> messageIds = new List<string>();
+
+            allMessages = OpenPopParser.getAllMessages();
+            messageIds.Add(allMessages[0].Headers.MessageId);
+            messageIds.Add(allMessages[1].Headers.MessageId);
+            messageIds.Add(allMessages[2].Headers.MessageId);
+            messageIds.Add(allMessages[3].Headers.MessageId);
+            messageIds.Add(allMessages[4].Headers.MessageId);
+
+            OpenPop.Pop3.Pop3Client client = new OpenPop.Pop3.Pop3Client();
+            foreach (var item in messageIds)
+            {
+                client.Connect("pop.gmail.com", 995, true);
+                client.Authenticate("recent:" + Users.username, Users.password);
+                if (OpenPopParser.DeleteMessageByMessageId(client, item))
+                {
+                    client.Disconnect();
+                }
+            }
+        }
+    }
+}
