cd27e353082bcb02462b15b2010c0dcf1a873d6c
Xoferif <Sniffern@msn.com>
2014-12-10 09:30:42 +0100

    xml parser inc comments.

--- MailClient/LocalDataCache.cs
+++ MailClient/LocalDataCache.cs
@@ -10,11 +10,17 @@ namespace MailClient
 {
     public partial class LocalDataCache
     {
+        // Folderlocation for the emails
         private static string xmlLocation = @"c:\ProgramData\MailClient\";
+        /// <summary>
+        /// saving all mails in two xml files, incomming and sent.
+        /// </summary>
         public static void saveMailsToXml()
         {
+            // check if directory exists
             if (Directory.Exists(xmlLocation))
             {
+                //check if files exist
                 if (File.Exists(xmlLocation + "incommingMails.xml"))
                 {
                     File.Delete(xmlLocation + "incommingMails.xml");
@@ -25,26 +31,86 @@ namespace MailClient
                 }
 
             }
+            //create directory if it does not exist.
             else
             {
                 Directory.CreateDirectory(xmlLocation);
             }
-
-            using (XmlWriter writer = XmlWriter.Create(xmlLocation + "AllMails.xml"))
+            //create lists for the mails
+            List<OpenPop.Mime.Message> incommingMails = new List<OpenPop.Mime.Message>();
+            List<OpenPop.Mime.Message> sentMails = new List<OpenPop.Mime.Message>();
+            //populate the lists with emails
+            incommingMails = OpenPopParser.getIncommingOrSentMessages("incomming");
+            sentMails = OpenPopParser.getIncommingOrSentMessages("sent");
+            // initializes xml writer
+            using (XmlWriter writer = XmlWriter.Create(xmlLocation + "incommingMails.xml"))
             {            
-                writer.WriteStartElement("RegExes");
-                foreach (var RegEx in RegExesList)
+                //making the first element.
+                writer.WriteStartElement("Incomming Emails");
+                //running through all the emails
+                foreach (var Email in incommingMails)
                 {
-                    writer.WriteStartElement("RegEx");
-                    writer.WriteElementString("Name", RegEx.Name);
-                    writer.WriteElementString("Expression", RegEx.Expression);
-                    writer.WriteEndElement();
+                    //fetching the body of the mail.
+                    string body = "";
+                    OpenPop.Mime.MessagePart bodyPart = Email.FindFirstHtmlVersion();
+                    if (bodyPart != null)
+                    {
+                        body = bodyPart.GetBodyAsText();
+                    }
+                    else
+                    {
+                        bodyPart = Email.FindFirstPlainTextVersion();
+                        if (bodyPart != null)
+                        {
+                            body = bodyPart.GetBodyAsText();
+                        }
+                    }
+                    writer.WriteStartElement("Email");                                      //creating the next element in xml
+                    writer.WriteElementString("From", Email.Headers.From.ToString());       //writing 'From'
+                    writer.WriteElementString("To", Email.Headers.To[0].ToString());        //writing 'To'
+                    writer.WriteElementString("Subject", Email.Headers.Subject.ToString()); //writing 'subject'
+                    writer.WriteElementString("Time", Email.Headers.DateSent.ToString());   //writing 'TimeSent'
+                    writer.WriteElementString("Body", body );                               //writing 'body'
+                    writer.WriteEndElement();                                               //ending element
                 }
-                writer.WriteEndElement();
-                writer.Flush();
+                writer.WriteEndElement();                                                   //ending element           
+                writer.Flush();                                                             //writes all data from buffer
             }
-   
-        }
 
+
+            using (XmlWriter writer = XmlWriter.Create(xmlLocation + "sentMails.xml"))
+            {
+                //making the first element.
+                writer.WriteStartElement("Sent Emails");
+                //running through all the emails
+                foreach (var Email in sentMails)
+                {
+                    //fetching the body of the mail.
+                    string body = "";
+                    OpenPop.Mime.MessagePart bodyPart = Email.FindFirstHtmlVersion();
+                    if (bodyPart != null)
+                    {
+                        body = bodyPart.GetBodyAsText();
+                    }
+                    else
+                    {
+                        bodyPart = Email.FindFirstPlainTextVersion();
+                        if (bodyPart != null)
+                        {
+                            body = bodyPart.GetBodyAsText();
+                        }
+                    }
+                    writer.WriteStartElement("Email");                                      //creating the next element in xml
+                    writer.WriteElementString("From", Email.Headers.From.ToString());       //writing 'From'
+                    writer.WriteElementString("To", Email.Headers.To[0].ToString());        //writing 'To'
+                    writer.WriteElementString("Subject", Email.Headers.Subject.ToString()); //writing 'subject'
+                    writer.WriteElementString("Time", Email.Headers.DateSent.ToString());   //writing 'TimeSent'
+                    writer.WriteElementString("Body", body );                               //writing 'body'
+                    writer.WriteEndElement();                                               //ending element
+                }
+                writer.WriteEndElement();                                                   //ending element           
+                writer.Flush();                                                             //writes all data from buffer
+            }
+        }
     }
 }
